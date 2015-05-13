8239daa32a8d1f9732a4f1fe0f6b6300a56c7f17
Jose M. Chumo Mata <jmchuma@gmail.com>
2015-05-13 11:33:41 +0200

    Display entries using a fragment

---
+++ src/main/java/com/xchuma/increpaste/ui/AllEntriesFragment.java
@@ -0,0 +1,99 @@
+package com.xchuma.increpaste.ui;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.xchuma.increpaste.R;
+import com.xchuma.increpaste.persistence.Entry;
+import com.xchuma.increpaste.persistence.EntryDS;
+
+import java.sql.SQLException;
+
+public class AllEntriesFragment extends Fragment implements View.OnClickListener {
+
+    private final String TAG = AllEntriesFragment.class.getName();
+    private OnNewEntryListener _callback;
+
+
+    @Override /* Fragment */
+    public void onActivityCreated(Bundle savedState) {
+        super.onActivityCreated(savedState);
+
+        Button button = (Button) getView().findViewById(R.id.button_editEntry);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // Ensure that the activity implements the callback interface.
+        try {
+            _callback = (OnNewEntryListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnNewEntryListener");
+        }
+    }
+
+    @Override /* View.OnClickListener */
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_editEntry:
+                _callback.onNewEntry();
+                break;
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_entries, container, false);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        Activity activity = getActivity();
+        View view = getView();
+        int pos = 0;
+
+        try {
+            EntryDS ds = new EntryDS(activity);
+            ds.open();
+
+            StringBuilder strBuilder = new StringBuilder();
+            for(Entry entry : ds.read()) {
+                strBuilder.append(entry.getText()+"\n");
+                if(entry.getPos() > pos) pos = entry.getPos();
+            }
+            TextView textView = (TextView) view.findViewById(R.id.textView_entries);
+            textView.setText(strBuilder);
+
+            ds.close();
+        } catch (SQLException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        SharedPreferences entry_extra = activity.getSharedPreferences("ENTRY_EXTRA", 0);
+        entry_extra.edit().putInt("LAST_POST", pos).apply();
+    }
+
+
+    public interface OnNewEntryListener {
+        void onNewEntry();
+    }
+}
--- src/main/java/com/xchuma/increpaste/ui/MainActivity.java
+++ src/main/java/com/xchuma/increpaste/ui/MainActivity.java
@@ -1,36 +1,23 @@
 package com.xchuma.increpaste.ui;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.xchuma.increpaste.R;
-import com.xchuma.increpaste.persistence.Entry;
 import com.xchuma.increpaste.persistence.EntryDS;
 
 import java.sql.SQLException;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends ActionBarActivity
+        implements AllEntriesFragment.OnNewEntryListener {
 
     private final String TAG = MainActivity.class.getName();
 
-    @Override /* View.OnClickListener */
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button_editEntry:
-                Intent i = new Intent().setClass(this, EditEntryActivity.class);
-                startActivity(i);
-                break;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,9 +40,6 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
 
         setContentView(R.layout.activity_main);
-
-        Button button = (Button) findViewById(R.id.button_editEntry);
-        button.setOnClickListener(this);
     }
 
 
@@ -64,6 +48,12 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override /* AllEntriesFragment.OnNewEntryListener */
+    public void onNewEntry() {
+        Intent i = new Intent().setClass(this, EditEntryActivity.class);
+        startActivity(i);
     }
 
     @Override
@@ -79,32 +69,5 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        int pos = 0;
-
-        try {
-            EntryDS ds = new EntryDS(this);
-            ds.open();
-
-            StringBuilder strBuilder = new StringBuilder();
-            for(Entry entry : ds.read()) {
-                strBuilder.append(entry.getText()+"\n");
-                if(entry.getPos() > pos) pos = entry.getPos();
-            }
-            TextView textView = (TextView) findViewById(R.id.textView_entries);
-            textView.setText(strBuilder);
-
-            ds.close();
-        } catch (SQLException e) {
-            Log.e(TAG, e.getMessage());
-        }
-
-        SharedPreferences entry_extra = getSharedPreferences("ENTRY_EXTRA", 0);
-        entry_extra.edit().putInt("LAST_POST", pos).commit();
     }
 }
