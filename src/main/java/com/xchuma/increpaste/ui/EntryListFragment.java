102d4504f32ece0134b5a934348c0e633c53cac8
Jose M. Chumo Mata <jmchuma@gmail.com>
2015-08-24 09:24:37 +0200

    Capture long clicks on the list
    * Activate multiple choices on the list in modal selection mode.
    * Listen to long click and handle the selections in modal mode.
    * Add the modal menu elements even though they are not functional yet.

--- src/main/java/com/xchuma/increpaste/ui/EntryListFragment.java
+++ src/main/java/com/xchuma/increpaste/ui/EntryListFragment.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.app.ListFragment;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.xchuma.increpaste.R;
 
@@ -32,6 +36,81 @@ public class EntryListFragment extends ListFragment {
      * fragment (e.g. upon screen orientation changes).
      */
     public EntryListFragment() {
+    }
+
+    @Override
+    public void onActivityCreated(Bundle saved) {
+        super.onActivityCreated(saved);
+
+        final ListView listView =  getListView();
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                listView.setItemChecked(pos, true);
+                return true;
+            }
+        });
+
+        listView.setMultiChoiceModeListener(new ListView.MultiChoiceModeListener() {
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_copy:
+                        Toast.makeText(getActivity(), "Copied to clipboard " + listView.getCheckedItemCount() +
+                                " items", Toast.LENGTH_SHORT).show();
+                        mode.finish();
+                        break;
+
+                    case R.id.action_delete:
+                        Toast.makeText(getActivity(), "Deleted " + listView.getCheckedItemCount() +
+                                " items", Toast.LENGTH_SHORT).show();
+                        mode.finish();
+                        break;
+
+                    default:
+                        Toast.makeText(getActivity(), "Clicked " + item.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                getActivity().getMenuInflater().inflate(
+                        R.menu.menu_entry_list_fragment_context, menu);
+                mode.setSubtitle("One item selected");
+                return true;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int pos,
+                                                  long id, boolean checked) {
+                final int count = listView.getCheckedItemCount();
+                switch (count) {
+                    case 0:
+                        mode.setSubtitle(null);
+                        break;
+                    case 1:
+                        mode.setSubtitle("One item selected");
+                        break;
+                    default:
+                        mode.setSubtitle("" + count + " items selected");
+                        break;
+                }
+            }
+        });
     }
 
     @Override
